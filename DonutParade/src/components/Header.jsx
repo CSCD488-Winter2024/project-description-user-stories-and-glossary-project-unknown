c1c48bb6c365d218a5d36880ccc44daa723d2869
Roberto Garcia <129777137+robertogarcia11@users.noreply.github.com>
2024-04-29 03:28:28 +0000

    merged Navbar.jsx into Header.jsx

--- DonutParade/src/components/Header.jsx
+++ DonutParade/src/components/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import Navbar from './Navbar'
 import DonutParadeLogo from '../assets/DonutParadeLogoblack.png'
+import Cart from '../assets/Cart.png'
+import User from '../assets/User.png'
 import '../styles/Header.css'
 
 function Header() {
   return (
+
     <div className='Header'>
-      
-        
-        <a href='/'><img src={DonutParadeLogo} alt="Donut Parade Logo" id='DonutParadeLogo' /></a>
-        <Navbar />
-        
-      
+      <a href='/'><img src={DonutParadeLogo} alt="Donut Parade Logo" id='DonutParadeLogo' /></a>
+
+      <div id ='Navbar'>
+        <ul>
+          <li><a href="/menu"><p>Menu</p></a></li>
+          <li><a href="/contact"><p>Contact</p></a></li>
+          <li><a href="/about"><p>About</p></a></li>
+          <li><a href="/cart"><img src={Cart} alt="Cart" id='Cart' /></a></li>
+          <li><a href="/login"><img src={User} alt='User' id='User'></img></a></li>
+        </ul>
+      </div>
+
     </div>
+    
   )
 }
 

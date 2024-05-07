55839c947962928695b020ea2123b336a5417069
Roberto Garcia <129777137+robertogarcia11@users.noreply.github.com>
2024-05-07 04:25:21 +0000

    editing contact page

--- DonutParade/src/pages/Contact.jsx
+++ DonutParade/src/pages/Contact.jsx
@@ -10,37 +10,6 @@ function Contact() {
 
       <Header />
 
-      <div class="month">
-        <ul>
-        <li class="prev">&#10094;</li>
-        <li class="next">&#10095;</li>
-        <li>August<br/><span>2021</span></li>
-        </ul>
-      </div>
-
-      <ul class="weekdays">
-        <li>Su</li>
-        <li>Mo</li>
-        <li>Tu</li>
-        <li>We</li>
-        <li>Th</li>
-        <li>Fr</li>
-        <li>Sa</li>
-      </ul>
-
-      <ul class="days">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li><span class="active">10</span></li>
-        <li>11</li>
-      </ul>
       <section id='contactContainer'>
         <h1 id='contact'>Contact Us</h1>
         <img src={DonutShop} alt="Shop" />

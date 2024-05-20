bdb46e395ffe13bebaa6c922f14bf0fd7e1b24da
Roberto Garcia <129777137+robertogarcia11@users.noreply.github.com>
2024-05-20 03:06:37 +0000

    forgot-password-page-css

--- DonutParade/src/pages/ForgotPassword.jsx
+++ DonutParade/src/pages/ForgotPassword.jsx
@@ -10,7 +10,8 @@ function ForgotPassword() {
         <Header />
 
         <div id='forgotContainer'>
-            <h1>Forgot Password</h1>
+            <h1>Forgot Password?</h1>
+            <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2lld3ZsY21sa2V5dXNyMndtOTk2c2llZWVrYml4ZjIzYXkyeWpoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26BROFLJSFhP0cMGk/giphy.gif" alt="meme" />
             <p>Enter your email address and we'll send you a link to reset your password.</p>
             <form>
                 <input id='email' type='email' placeholder='Email' required />

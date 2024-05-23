0893317eb60edeca0691abeaa3d4c4afe1c705f0
Rachael DeBoer <145617897+rkdeboer@users.noreply.github.com>
2024-05-23 22:59:03 +0000

    Map styling

--- DonutParade/src/pages/Contact.jsx
+++ DonutParade/src/pages/Contact.jsx
@@ -14,7 +14,7 @@ function Contact() {
         
         
         
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5372.693359511857!2d-117.39888412373733!3d47.67768688306616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549e18dcc8daab87%3A0xe1ce23d8a13f20e0!2sDonut%20Parade!5e0!3m2!1sen!2sus!4v1716504765524!5m2!1sen!2sus" width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5372.693359511857!2d-117.39888412373733!3d47.67768688306616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549e18dcc8daab87%3A0xe1ce23d8a13f20e0!2sDonut%20Parade!5e0!3m2!1sen!2sus!4v1716504765524!5m2!1sen!2sus" width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <div class="side-text">
         <p>2152 N Hamilton St, Spokane WA</p>
         <p id='phone-number'>

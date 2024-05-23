6c863d21e3d542cd9069130c6d045bf4321ae29b
Rachael DeBoer <145617897+rkdeboer@users.noreply.github.com>
2024-05-23 22:50:46 +0000

    Change hover effect

--- DonutParade/src/pages/Contact.jsx
+++ DonutParade/src/pages/Contact.jsx
@@ -19,7 +19,7 @@ function Contact() {
         <p id='phone-number'>
           <a id="phone-number-button" href="tel:+15094739870">(509) 473-9870</a>
         </p>
-        <p>For all inquiries, please email us at <a href='mailto:donutparadespokane@gmail.com' style= {{textDecoration: 'underline'}}>donutparadespokane@gmail.com</a></p>
+        <p>For all inquiries, please email us at <a id="email" href='mailto:donutparadespokane@gmail.com'>donutparadespokane@gmail.com</a></p>
         </div>
         
       </section>

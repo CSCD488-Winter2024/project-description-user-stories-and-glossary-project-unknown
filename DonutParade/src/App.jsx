aea4bc2c8d7f4d36deb7ca5042d786ad0edb93b4
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2024-05-29 13:53:28 -0700

    Update App.jsx

--- DonutParade/src/App.jsx
+++ DonutParade/src/App.jsx
@@ -1,3 +1,21 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider, Route, Link, Outlet } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import User from './pages/User';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import ErrorPage from './pages/ErrorPage';
+import AdminOrders from './pages/AdminOrders';
+import AdminEdit from './pages/AdminEdit';
+import AdminHome from './pages/AdminHome';
+import { CartProvider } from './components/CartContext';
+import { AuthProvider } from './pages/AuthContext'; // Import AuthProvider
+import AdminRoute from './pages/AdminRoute'; // Import the AdminRoute component
 const routes = [
   {
     path: '/',
